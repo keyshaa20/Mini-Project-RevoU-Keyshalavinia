7a986d97e2565c24aee19e15e21041d907c845b4
keyshaa20 <pckeysha@gmail.com>
2024-07-16 22:30:58 +0700

    update code

--- js/js.scripts.js
+++ js/js.scripts.js
@@ -1,1 +1,20 @@
 // ini javascript
+function calculate() {
+    var height = parseFloat(document.getElementById("height").value)/100;
+    var weight = parseFloat(document.getElementById("weight").value);
+    var result = weight / (height * height);
+    var keterangan ="-";
+
+    if(result > 27){
+        keterangan = "obese";
+    } else if ((result >= 25.1 ) & (result <= 27)){
+        keterangan =  "overweight";
+    } else if ((result >= 18.5 ) & (result <= 25)){
+        keterangan =  "normal";
+    } else if ((result >= 17 ) & (result <= 18.5)){
+        keterangan =  "underweight"
+    }
+
+    document.getElementById("result").innerHTML = result.toFixed(1);
+    document.getElementById("keterangan").innerHTML = keterangan;
+}
